70075a6fd6249fcded932d90b1c8c647fb8b8d53
Anonimous-User <95382802+Anonimous-User@users.noreply.github.com>
2022-09-03 19:15:51 -0400

    added smth

---
+++ Server/Controller/SmthController.cs
@@ -0,0 +1,25 @@
+using Ecohacks.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecohacks.Server.Controllers;
+
+[ApiController]
+[Route("Demo")]
+public class DemoController : ControllerBase
+{
+    [HttpPost]
+    [Route("StoreToDb")]
+    public async Task<ActionResult> Store([FromBody] DbObjectKv keyValuePair)
+    {
+        await Task.Delay(69);
+        return Ok();
+    }
+	
+    [HttpGet]
+    [Route("GetFromDb")]
+    public async Task<ActionResult> Get()
+    {
+        await Task.Delay(69);
+        return Ok();
+    }
+}
