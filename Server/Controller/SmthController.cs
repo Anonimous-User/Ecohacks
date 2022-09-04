00e5bff5f126364af820f0e81e423add51beb2d3
Anonimous-User <95382802+Anonimous-User@users.noreply.github.com>
2022-09-03 20:09:54 -0400

    connected to database

--- Server/Controller/SmthController.cs
+++ Server/Controller/SmthController.cs
@@ -1,25 +1,6 @@
-using Ecohacks.Shared;
-using Microsoft.AspNetCore.Mvc;
-
-namespace Ecohacks.Server.Controllers;
-
-[ApiController]
-[Route("Demo")]
-public class DemoController : ControllerBase
+string query = "SELECT * FROM residential_freezers WHERE ModelNumber = @Query";
+IConnector conn = new Connector();
+List<Model> results = await conn.QueryAsync<Model, dynamic>(query, new
 {
-    [HttpPost]
-    [Route("StoreToDb")]
-    public async Task<ActionResult> Store([FromBody] DbObjectKv keyValuePair)
-    {
-        await Task.Delay(69);
-        return Ok();
-    }
-	
-    [HttpGet]
-    [Route("GetFromDb")]
-    public async Task<ActionResult> Get()
-    {
-        await Task.Delay(69);
-        return Ok();
-    }
-}
+    Query = userInputModelNumber
+});
